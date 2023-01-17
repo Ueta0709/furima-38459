48bfe415f7cb13a0c142b4e1dc69d90a7e3ded4f
Ueta0709 <113412485+Ueta0709@users.noreply.github.com>
2023-01-17 20:17:53 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :edit_move_to_index, only: [:edit]
 
 def index
   @items = Item.all.order("created_at DESC")
@@ -39,6 +40,13 @@ private
 
 def item_params
   params.require(:item).permit(:title,:text,:image,:category_id,:condition_id, :shipping_fee_id, :shipping_area_id, :shipping_period_id, :price).merge(user_id: current_user.id)
+end
+
+def edit_move_to_index
+  @item_id = Item.find(params[:id])
+  unless user_signed_in? && current_user.id == @item_id.user_id 
+    redirect_to action: :index
+  end
 end
 
 end
