1359fdf60a0036ef10fa154b5608cc9d2b3a16d9
Ueta0709 <113412485+Ueta0709@users.noreply.github.com>
2023-01-19 21:11:16 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,8 +37,10 @@ def update
 end
 
 def destroy
-  @item.destroy
-  redirect_to root_path
+  if current_user.id == @item.user_id
+    @item.destroy
+    redirect_to root_path
+  end
 end
 
 private
